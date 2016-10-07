a3d63500bd395c2e2180151f084ae291e03f9bd3
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-10-07 12:51:11 +0200

    modified Developer class

--- src/main/java/de/fh_kiel/Person/Developer.java
+++ src/main/java/de/fh_kiel/Person/Developer.java
@@ -4,5 +4,10 @@ package de.fh_kiel.Person;
  * Created by Ankit on 10/7/2016.
  */
 public class Developer extends Person {
-
+    private float WorkExp;
+    private double Salary;
+    private String[] prog_lang = new String[12];
+ //   public void sample (){
+  //      System.out.println("checkVarible :" + getLast_Name());
+   // }
 }
