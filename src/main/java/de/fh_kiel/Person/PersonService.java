2465be1e9ad016adfb84123412402d951e9a2794
Ankit Mishra <ankitmishra.kcbct@gmail.com>
2016-10-07 14:47:13 +0200

    updating main method

--- src/main/java/de/fh_kiel/Person/PersonService.java
+++ src/main/java/de/fh_kiel/Person/PersonService.java
@@ -6,15 +6,17 @@ import java.time.LocalDate;
  */
 public class PersonService {
 
-
-
     // Main Method
     public static void main(String args[]){
-        Person person = new Person("Amit","Nagar", LocalDate.of(1900,1,01),"Male");
+        Person person = new Person("Amit","Nagar", LocalDate.of(1980,1,01),"Male");
+
+
+        Developer developer = new Developer();
+
 
         System.out.println(person.getFirst_Name() + " " +person.getLast_Name() +" "+ person.getAge() +" "+ person.getGender());
         System.out.println("tostring :" + person.toString());
-        System.out.println("Hash :" + person.hashCode());
+        System.out.println("Hash :" + person.hashCode()+"  "+developer.getFirst_Name());
     }
 
 }
