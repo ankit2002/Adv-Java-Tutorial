8df296bdd23328e83c4b2a3e899f5805674da142
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-11-11 15:00:00 +0100

    static method for company initial data

--- src/main/java/de/fh_kiel/person/datamodel/CompanyDAOImpl.java
+++ src/main/java/de/fh_kiel/person/datamodel/CompanyDAOImpl.java
@@ -4,14 +4,14 @@ package de.fh_kiel.person.datamodel;
 
 import de.fh_kiel.person.exception.CompanyNotFound;
 import de.fh_kiel.person.stubclass.Company;
+import de.fh_kiel.person.stubclass.Gender;
+import de.fh_kiel.person.stubclass.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.time.LocalDate;
+import java.util.*;
 
 /**
  * Created by Ankit on 10/25/2016.
@@ -22,7 +22,31 @@ public class CompanyDAOImpl implements CompanyDAO {
     /**
      * HashSet to store Companies
      */
-    private final Set<Company> companiesList = new HashSet<>();
+    private static final Set<Company> companiesList = new HashSet<>();
+
+    static {
+        Person person = new Person();
+        person.setId(1L);
+        person.setFirst_Name("Free");
+        person.setLast_Name("Falling");
+        person.setD_o_b(LocalDate.of(1983, 10, 10));
+        person.setGender(Gender.Male);
+        Person person1 = new Person();
+        person1.setId(2L);
+        person1.setFirst_Name("Amit");
+        person1.setLast_Name("nagar");
+        person1.setD_o_b(LocalDate.of(1983, 10, 10));
+        person1.setGender(Gender.Male);
+        Person person3 = new Person();
+        person3.setId(3L);
+        person3.setFirst_Name("hello");
+        person3.setLast_Name("guys");
+        person3.setD_o_b(LocalDate.of(1983, 10, 10));
+        person3.setGender(Gender.Male);
+        companiesList.add(new Company(1, "Company1", new ArrayList<Person>(Arrays.asList(person))));
+        companiesList.add(new Company(2, "Company2", new ArrayList<Person>(Arrays.asList(person1))));
+        companiesList.add(new Company(3, "Company3", new ArrayList<Person>(Arrays.asList(person3))));
+    }
 
     /**
      * Logger Variable
