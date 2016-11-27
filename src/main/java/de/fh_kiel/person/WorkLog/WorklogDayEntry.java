ce75d88ac712b08969b2b7f992a9261a05d1b1a0
Ankit Mishra <ankitmishra.kcbct@gmail.com>
2016-11-27 22:20:04 +0100

    removed annotation dependency and added two class

---
+++ src/main/java/de/fh_kiel/person/WorkLog/WorklogDayEntry.java
@@ -0,0 +1,12 @@
+package de.fh_kiel.person.WorkLog;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * Created by Ankit on 11/27/2016.
+ */
+public class WorklogDayEntry {
+    LocalDate day;
+    List<WorklogTimeEntry> entries;
+}
---
+++ src/main/java/de/fh_kiel/person/WorkLog/WorklogTimeEntry.java
@@ -0,0 +1,11 @@
+package de.fh_kiel.person.WorkLog;
+
+import java.time.LocalTime;
+
+/**
+ * Created by Ankit on 11/27/2016.
+ */
+public class WorklogTimeEntry {
+    LocalTime begin;
+    LocalTime end;
+}
