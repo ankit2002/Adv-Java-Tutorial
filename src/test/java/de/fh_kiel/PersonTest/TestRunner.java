9581e78da9a6bd0caf11e2df274b9ee4b8151d81
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-11-28 01:18:56 +0100

    Added Runner Class for Junit testing

---
+++ src/test/java/de/fh_kiel/PersonTest/TestRunner.java
@@ -0,0 +1,20 @@
+package de.fh_kiel.PersonTest;
+
+/**
+ * Created by amit on 28.11.16.
+ */
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+    public static void main(String[] args) {
+        Result result = JUnitCore.runClasses(filterValuesTest.class);
+
+        for (Failure failure : result.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result.wasSuccessful());
+    }
+}
