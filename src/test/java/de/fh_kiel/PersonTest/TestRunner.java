61daf3bbc1741e1911702d95981fd7ef2d5adc93
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-11-28 15:58:43 +0100

    Modified Test runned class to run both filtervalues and person service test class

--- src/test/java/de/fh_kiel/PersonTest/TestRunner.java
+++ src/test/java/de/fh_kiel/PersonTest/TestRunner.java
@@ -3,18 +3,33 @@ package de.fh_kiel.PersonTest;
 /**
  * Created by amit on 28.11.16.
  */
+
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
+
 public class TestRunner {
     public static void main(String[] args) {
-        Result result = JUnitCore.runClasses(filterValuesTest.class);
+
+        /*TestSuite suite = new TestSuite(filterValuesTest.class, PersonServiceTest.class);
+        TestResult result = new TestResult();
+        suite.run(result);
+        System.out.println("Number of test cases = " + result.runCount());*/
+        Result result = JUnitCore.runClasses(PersonServiceTest.class);
 
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
         }
 
         System.out.println(result.wasSuccessful());
+
+        Result result1 = JUnitCore.runClasses(filterValuesTest.class);
+
+        for (Failure failure : result1.getFailures()) {
+            System.out.println(failure.toString());
+        }
+
+        System.out.println(result1.wasSuccessful());
     }
 }
