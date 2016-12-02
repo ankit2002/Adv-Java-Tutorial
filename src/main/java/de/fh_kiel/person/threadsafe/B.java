f3069df11fe7f6c667ee2563fed0b439029fea20
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-12-02 17:29:57 +0100

    Task 1b

---
+++ src/main/java/de/fh_kiel/person/threadsafe/B.java
@@ -0,0 +1,27 @@
+package de.fh_kiel.person.threadsafe;
+
+/**
+ * Created by amit on 01.12.16.
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class B {
+    private final List<Integer> ints = new ArrayList<>();
+
+    public synchronized List<Integer> getInts() {
+        //System.out.println("inside get");
+        return ints;
+    }
+
+    public synchronized void addInt(final Integer intt) {
+        //System.out.println("inside add");
+
+        try {
+            Thread.sleep(intt);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        ints.add(intt);
+    }
+}
---
+++ src/main/java/de/fh_kiel/person/threadsafe/MainB.java
@@ -0,0 +1,33 @@
+package de.fh_kiel.person.threadsafe;
+
+/**
+ * Created by amit on 01.12.16.
+ */
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class MainB {
+
+    public static void main(String[] args) {
+        final B b = new B();
+        System.out.println("inside main");
+        IntStream ints = new Random().ints(50000, 1, 10);
+        //System.out.println("insts:" + ints);
+        List<CompletableFuture<Void>> collect = ints.mapToObj(i -> doSth(b, i)).collect(Collectors.toList());
+        System.out.println("collect:" + collect);
+        CompletableFuture.allOf((CompletableFuture[])collect.toArray(new CompletableFuture[0]));
+        System.out.println(b.getInts().size());
+    }
+
+    private static CompletableFuture<Void> doSth(final B b, final int i) {
+        //System.out.println("inside void");
+        return CompletableFuture.runAsync(() -> {
+            if (!b.getInts().contains(i)) {
+                b.addInt(i);
+            }
+        });
+    }
+}
