ace5d76e51daa72d123195cc8bd22f1d9eb826ce
Amit Nagar <amit.k.nagar@student.fh-kiel.de>
2016-12-17 00:42:23 +0100

    Modified the loading class to solve language mapping issue

--- src/main/java/de/fh_kiel/person/initializeTables/InitialTableLoad.java
+++ src/main/java/de/fh_kiel/person/initializeTables/InitialTableLoad.java
@@ -2,10 +2,9 @@ package de.fh_kiel.person.initializeTables;
 
 import de.fh_kiel.person.repositories.CompanyRepository;
 import de.fh_kiel.person.repositories.PersonRepository;
+import de.fh_kiel.person.repositories.ProgLangRepository;
 import de.fh_kiel.person.repositories.WorkLogRepository;
-import de.fh_kiel.person.stubclass.Company;
-import de.fh_kiel.person.stubclass.Gender;
-import de.fh_kiel.person.stubclass.ProjectManager;
+import de.fh_kiel.person.stubclass.*;
 import de.fh_kiel.person.worklog.WorklogDayEntry;
 import de.fh_kiel.person.worklog.WorklogTimeEntry;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,15 +31,19 @@ public class InitialTableLoad implements CommandLineRunner {
     @Autowired
     private CompanyRepository companyRepository;
 
-    /*@Autowired
+    @Autowired
     private ProgLangRepository progLangRepository;
-*/
 
     @Override
     public void run(String... args) throws Exception {
 
+        /**
+         * Project Manager details loaded
+         */
+
         ProjectManager p1 = new ProjectManager();
         ProjectManager p2 = new ProjectManager();
+
 
         p1.setFirst_Name("Amit");
         p1.setLast_Name("Nagar");
@@ -62,6 +65,9 @@ public class InitialTableLoad implements CommandLineRunner {
 
         p2.setPmExp(10);
 
+        /**
+         * Project manager Work log loaded
+         */
         WorklogDayEntry w1 = new WorklogDayEntry();
         WorklogDayEntry w2 = new WorklogDayEntry();
         WorklogDayEntry w3 = new WorklogDayEntry();
@@ -99,7 +105,77 @@ public class InitialTableLoad implements CommandLineRunner {
         w3.setPerson(p2);
         w4.setPerson(p2);
 
-       /* ProgLanguage pl1 = new ProgLanguage();
+        /**
+         * Developer details loaded
+         */
+
+        Developer d1 = new Developer();
+        Developer d2 = new Developer();
+
+
+        d1.setFirst_Name("developer1");
+        d1.setLast_Name("Nagar");
+        d1.setD_o_b(LocalDate.of(1999,10,03));
+        d1.setGender(Gender.Male);
+
+        d1.setWorkExp(4);
+        d1.setMin_Salary(500.00);
+
+        d2.setFirst_Name("developer2");
+        d2.setLast_Name("Nagar");
+        d2.setD_o_b(LocalDate.of(2000,10,03));
+        d2.setGender(Gender.Female);
+
+        d2.setWorkExp(4);
+        d2.setMin_Salary(500.00);
+
+        /**
+         * Developer Woke log loaded
+         */
+
+        WorklogDayEntry w5 = new WorklogDayEntry();
+        WorklogDayEntry w6 = new WorklogDayEntry();
+        WorklogDayEntry w7 = new WorklogDayEntry();
+        WorklogDayEntry w8 = new WorklogDayEntry();
+
+
+        WorklogTimeEntry wt5 = new WorklogTimeEntry();
+        WorklogTimeEntry wt6 = new WorklogTimeEntry();
+        WorklogTimeEntry wt7 = new WorklogTimeEntry();
+        WorklogTimeEntry wt8 = new WorklogTimeEntry();
+
+
+        wt5.setBegin(LocalTime.MIN);
+        wt5.setEnd(LocalTime.MAX);
+        wt6.setBegin(LocalTime.MIN);
+        wt6.setEnd(LocalTime.MAX);
+        wt7.setBegin(LocalTime.MIN);
+        wt7.setEnd(LocalTime.MAX);
+        wt8.setBegin(LocalTime.MIN);
+        wt8.setEnd(LocalTime.MAX);
+
+        w5.setDay(LocalDate.now());
+        w6.setDay(LocalDate.now());
+        w7.setDay(LocalDate.now());
+        w8.setDay(LocalDate.now());
+
+
+        w5.getEntries().add(wt5);
+        w6.getEntries().add(wt6);
+        w7.getEntries().add(wt7);
+        w8.getEntries().add(wt8);
+
+        w5.setPerson(d1);
+        w6.setPerson(d1);
+        w7.setPerson(d2);
+        w8.setPerson(d2);
+
+
+        /**
+         * Programming Languages loaded for the Developer
+         */
+
+        ProgLanguage pl1 = new ProgLanguage();
         ProgLanguage pl2 = new ProgLanguage();
         ProgLanguage pl3 = new ProgLanguage();
         ProgLanguage pl4 = new ProgLanguage();
@@ -107,13 +183,23 @@ public class InitialTableLoad implements CommandLineRunner {
         pl2.setName("C++");
         pl3.setName("COBOL");
         pl4.setName("SAS");
+        pl1.getDeveloperlist().add(d1);
+        pl2.getDeveloperlist().add(d1);
+        pl1.getDeveloperlist().add(d2);
+        pl2.getDeveloperlist().add(d2);
 
-        p1.getProglanglist().add(pl1);
-        p1.getProglanglist().add(pl2);
-        p1.getProglanglist().add(pl3);
-        p1.getProglanglist().add(pl4);*/
 
 
+
+
+        d1.getProglanglist().add(pl1);
+        d1.getProglanglist().add(pl2);
+        d2.getProglanglist().add(pl1);
+        d2.getProglanglist().add(pl2);
+
+        /**
+         * Company details loaded for both developer and project managed
+         */
 
         Company cm1 = new Company();
         cm1.setCompanyName("Bosch");
@@ -121,32 +207,47 @@ public class InitialTableLoad implements CommandLineRunner {
         cm2.setCompanyName("Google");
         cm1.getCompanyEmpList().add(p1);
         cm1.getCompanyEmpList().add(p2);
+        cm2.getCompanyEmpList().add(d1);
+        cm2.getCompanyEmpList().add(d2);
 
         companyRepository.save(cm1);
         companyRepository.save(cm2);
 
+
         p1.setCompany(cm1);
         p2.setCompany(cm1);
+        d1.setCompany(cm2);
+        d2.setCompany(cm2);
 
         p1.getWorklogs().add(w1);
         p1.getWorklogs().add(w2);
         p2.getWorklogs().add(w3);
         p2.getWorklogs().add(w4);
 
+        d1.getWorklogs().add(w5);
+        d1.getWorklogs().add(w6);
+        d2.getWorklogs().add(w7);
+        d2.getWorklogs().add(w8);
+
         personRepository.save(p1);
         personRepository.save(p2);
+        personRepository.save(d1);
+        personRepository.save(d2);
 
         workLogRepository.save(w1);
         workLogRepository.save(w2);
         workLogRepository.save(w3);
         workLogRepository.save(w4);
+        workLogRepository.save(w5);
+        workLogRepository.save(w6);
+        workLogRepository.save(w7);
+        workLogRepository.save(w8);
 
 
-       /* progLangRepository.save(pl1);
+        progLangRepository.save(pl1);
         progLangRepository.save(pl2);
         progLangRepository.save(pl3);
-        progLangRepository.save(pl4);*/
-
+        progLangRepository.save(pl4);
 
 
     }
